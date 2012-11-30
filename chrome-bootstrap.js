6881dea412a46d61eba585ba0e3749983864994e
Johann Pardanaud <pardanaud.j@gmail.com>
2012-11-30 14:31:21 +0100

    JavaScript indentation converted to 4 spaces

--- chrome-bootstrap.js
+++ chrome-bootstrap.js
@@ -1,55 +1,55 @@
 // This file needs the jQuery library or an alternative like Zepto
 
 $(function() {
-  
-  /* Navigation
-  ============================================== */
+    
+    /* Navigation
+    ============================================== */
 
-  $('.menu a').click(function(ev) {
-    ev.preventDefault();
-    var selected = 'selected';
+    $('.menu a').click(function(ev) {
+        ev.preventDefault();
+        var selected = 'selected';
 
-    $('.mainview > *').removeClass(selected);
-    $('.menu li').removeClass(selected);
-    setTimeout(function() {
-      $('.mainview > *:not(.selected)').css('display', 'none');
-    }, 100);
+        $('.mainview > *').removeClass(selected);
+        $('.menu li').removeClass(selected);
+        setTimeout(function() {
+            $('.mainview > *:not(.selected)').css('display', 'none');
+        }, 100);
 
-    $(ev.currentTarget).parent().addClass(selected);
-    var currentView = $($(ev.currentTarget).attr('href'));
-    currentView.css('display', 'block');
-    setTimeout(function() {
-      currentView.addClass(selected);
-    }, 0);
+        $(ev.currentTarget).parent().addClass(selected);
+        var currentView = $($(ev.currentTarget).attr('href'));
+        currentView.css('display', 'block');
+        setTimeout(function() {
+            currentView.addClass(selected);
+        }, 0);
 
-    setTimeout(function() {
-      $('body')[0].scrollTop = 0;
-    }, 200);
-  });
-
-  $('.mainview > *:not(.selected)').css('display', 'none');
-
-  /* Modals
-  ============================================== */
-
-  $('button[data-modal-target]').click(function(ev) {
-    ev.preventDefault();
-    var modal = $('#' + ev.target.dataset.modalTarget).clone();
-    $(modal).removeAttr('style');
-    $(modal).find('button').click(function() {
-      $(modal).addClass('transparent');
-      setTimeout(function() {
-        $(modal).remove();
-      }, 1000);
+        setTimeout(function() {
+            $('body')[0].scrollTop = 0;
+        }, 200);
     });
 
-    $(modal).click(function() {
-      $(modal).find('.page').addClass('pulse');
-      $(modal).find('.page').on('webkitAnimationEnd', function() {
-        $(this).removeClass('pulse');
-      });
+    $('.mainview > *:not(.selected)').css('display', 'none');
+
+    /* Modals
+    ============================================== */
+
+    $('button[data-modal-target]').click(function(ev) {
+        ev.preventDefault();
+        var modal = $('#' + ev.target.dataset.modalTarget).clone();
+        $(modal).removeAttr('style');
+        $(modal).find('button').click(function() {
+            $(modal).addClass('transparent');
+            setTimeout(function() {
+                $(modal).remove();
+            }, 1000);
+        });
+
+        $(modal).click(function() {
+            $(modal).find('.page').addClass('pulse');
+            $(modal).find('.page').on('webkitAnimationEnd', function() {
+                $(this).removeClass('pulse');
+            });
+        });
+        $('body').append(modal);
     });
-    $('body').append(modal);
-  });
 
 });
