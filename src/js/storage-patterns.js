a7ee850506e694b734310167448bb2eb26602cad
Johann Pardanaud <pardanaud.j@gmail.com>
2012-12-08 00:07:45 +0100

    The built-in patterns are now finished

--- src/js/storage-patterns.js
+++ src/js/storage-patterns.js
@@ -13,18 +13,38 @@ Storage
         }
     )
     
-    .setPattern('textarea', 'textarea', 'keyup', function() {
-            
-    })
+    .setPattern('textarea', 'textarea', 'keyup',
+        function() {
+            return this.value;
+        },
+        function(value) {
+            this.value = value;
+        }
+    )
     
-    .setPattern('radio', 'input[type=radio]', 'change', function() {
-            
-    })
+    .setPattern('radio', 'input[type=radio]', 'change',
+        function() {
+            return this.checked;
+        },
+        function(value) {
+            this.checked = value;
+        }
+    )
     
-    .setPattern('checkbox', 'input[type=checkbox]', 'change', function() {
-            
-    })
+    .setPattern('checkbox', 'input[type=checkbox]', 'change',
+        function() {
+            return this.checked;
+        },
+        function(value) {
+            this.checked = value;
+        }
+    )
     
-    .setPattern('select', 'select', 'change', function() {
-            
-    });
+    .setPattern('select', 'select', 'change',
+        function() {
+            return this.selectedIndex;
+        },
+        function(value) {
+            this.selectedIndex = value;
+        }
+    );
