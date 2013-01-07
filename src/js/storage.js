e82c7391e9ed199b56f8988616a0c7a99ee6214a
Johann Pardanaud <pardanaud.j@gmail.com>
2013-01-07 23:25:08 +0100

    The Storage object has been slightly modified to avoid the instanciation phase

--- src/js/storage.js
+++ src/js/storage.js
@@ -2,24 +2,24 @@
  * Storage class : declaration AND instanciation
  */
 
-var Storage = window.chromeBootstrap.Storage = (function() {
+var Storage = window.chromeBootstrap.Storage = {
 
-    var Storage = function() {
-        this.patterns = {};
+    /*
+     * Variables
+     */
+
+    patterns: {},
         
-        // Default parameters
-        this.params = {
-            'sync': true
-        };
-    };
-
-    var fn = Storage.prototype;
+    // Default parameters
+    params: {
+        'sync': true
+    },
 
     /*
      * Init
      */
 
-    fn.init = function(rootNode) {
+    init: function(rootNode) {
         // The "sync" param impacts on the storage area
         this.storageArea = this.params.sync
             ? chrome.storage.sync
@@ -32,26 +32,26 @@ var Storage = window.chromeBootstrap.Storage = (function() {
         nodes.forEach(function(node) {
             _this.bind(node);
         });
-    };
+    },
 
     /*
      * Parameters
      */
 
-    fn.setParam = function(id, value) {
+    setParam: function(id, value) {
         this.params[id] = value;
         return this; // Chaining
-    };
+    },
 
-    fn.getParam = function(id) {
+    getParam: function(id) {
         return this.params[id];
-    };
+    },
 
     /*
      * Patterns
      */
 
-    fn.setPattern = function(id, scheme, event, storeAction, loadAction) {
+    setPattern: function(id, scheme, event, storeAction, loadAction) {
         var _this = this,
             pattern = this.patterns[id] = {};
         
@@ -75,10 +75,10 @@ var Storage = window.chromeBootstrap.Storage = (function() {
         };
 
         return this; // Chaining
-    };
+    },
 
     // Returns the pattern that suits the given node, or null.
-    fn.getPattern = function(node) {
+    getPattern: function(node) {
         var patterns = this.patterns;
 
         for(var i in patterns) {
@@ -88,53 +88,47 @@ var Storage = window.chromeBootstrap.Storage = (function() {
         }
 
         return null;
-    };
+    },
 
-    fn.removePattern = function(id) {
+    removePattern: function(id) {
         this.patterns[id] = null;
-    };
+    },
 
     /*
      * Events
      */
 
-    fn.setBinding = function(node, method) {
+    setBinding: function(node, method) {
         var pattern = this.getPattern(node); // An event can't be added without a valid pattern
 
         pattern && node[method](pattern.event, pattern.storeEvent);
 
         return pattern;
-    }
+    },
 
-    fn.bind = function(node) {
+    bind: function(node) {
         var pattern = this.setBinding(node, 'addEventListener');
         pattern && pattern.loadEvent(node);
-    };
+    },
 
-    fn.unbind = function(node) {
+    unbind: function(node) {
         this.setBinding(node, 'removeEventListener');
-    };
+    },
 
     /*
      * Storage
      */
 
-    fn.store = function(id, value) {
+    store: function(id, value) {
         var items = {};
         items[id] = value;
         this.storageArea.set(items);
-    };
+    },
 
-    fn.load = function(id, callback) {
+    load: function(id, callback) {
         this.storageArea.get(id, function(items) {
             callback(items[id]);
         });
-    };
+    }
 
-    /*
-     * Object exposure
-     */
-
-    return new Storage();
-
-})();
+};
